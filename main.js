3a88ed620ffd1c6de37d53feef88fa029123571c
walkerdorris <walker.dorris@gmail.com>
2015-11-09 19:45:29 -0600

    added two way data binding

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var app = angular.module("TodoApp", [])
 
 app.controller("TodoCtrl", function($scope){
-	$scope.title = "Welcome to your Todo App"
+	$scope.title = "Welcome to your Todo App";
+})
+
+
+app.controller("PastaCtrl", function($scope){
+	$scope.pasta = "I love carbs";
+	$scope.foods = "";
 })
